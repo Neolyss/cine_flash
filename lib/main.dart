09458546c5f978647b7125679a468fb61b1ee5df
Jean-Philippe21 <jeanphilippeavossevou@gmail.com>
2021-12-23 22:57:03 +0100

    Les MAPs

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,27 @@ import 'package:cine_flash/result.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'header.dart';
+import 'package:camera/camera.dart';
 
-void main() {
-  runApp(const MyApp());
+
+
+Future<void> main()  async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final cameras = await availableCameras();//Acquisition de la list de cameras valide sur le périphérique
+
+  final firstCamera = cameras.first;
+  //WidgetsFluttterBinding.ensureInitialized();
+  runApp(MyApp(camera: firstCamera));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key,
+    required this.camera,
+  }) : super(key: key);
 
+  final CameraDescription camera;
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -25,7 +38,7 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: "/",
       routes: {
-        '/' : (context) => const MyHomePage(),
+        '/' : (context) => MyHomePage(camera : this.camera),
         '/result' : (context) => const ResultPage(),
       },
     );
@@ -33,7 +46,11 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({Key? key,
+    required this.camera,
+  }) : super(key: key);
+
+  final CameraDescription camera;
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
@@ -47,8 +64,27 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+class TakePictureScreen extends StatefulWidget {
+  const TakePictureScreen({Key? key,
+    required this.camera,
+  }) : super(key: key);
+
+  final CameraDescription camera;
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+}
+
+
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 1;
+
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
 
   void _onItemTapped(int index) {
     setState(() {
@@ -57,11 +93,45 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
+  void initState(){
+    super.initState();
+
+    _controller = CameraController(widget.camera ,ResolutionPreset.max,);
+
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose(){
+    _controller.dispose();
+    super.dispose();
+  }
+
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold (
       appBar: const WidgetAppBar(returnPage: false,),
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context,snapshot){
+          if(snapshot.connectionState == ConnectionState.done){
+            return CameraPreview(_controller);
+          }else{
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
+        onPressed: () async {
+          try{
+            await _initializeControllerFuture;
+
+            final image = await _controller.takePicture();
+
+          }catch(e){
+            print(e);
+          }
           Navigator.pushNamed(context, '/result');
         },
         tooltip: 'Increment',
@@ -92,9 +162,40 @@ class _MyHomePageState extends State<MyHomePage> {
         selectedItemColor: const Color(0xFF0E1111),
         onTap: _onItemTapped,
       ),// This trailing comma makes auto-formatting nicer for build methods.
+
       drawer: const WidgetDrawer(),
+
     );
 
 
   }
 }
+
+
+/*class TakePictureScreenState extends State<TakePictureScreen>{
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState(){
+    super.initState();
+
+    _controller = CameraController(widget.camera,ResolutionPreset.medium,);
+
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose(){
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Container();
+  }
+
+}*/
+
+
